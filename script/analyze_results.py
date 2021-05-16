4ae38606a5b9e510f7f4e0b9bed7a1b727aa7069
anhquan075 <19522081@gm.uit.edu.vn>
2021-05-16 02:42:52 +0000

    Add to merge

--- script/analyze_results.py
+++ script/analyze_results.py
@@ -6,28 +6,34 @@ import pandas as pd
 import dataframe_image as dfi
 
 
-def get_data(json_file, options=0):
+def get_data_errors(json_file, options=0):
     with open(json_file, 'r', encoding='utf-8') as f:
         datas = json.load(f)
-    
-    for data in datas:
-        if options == 1:
-            if not data['predicted_entities']:
-                print(data['text'])
-        elif options == 2:
-            if not data['entities']:
-                print(data['text'])
-        elif options == 3:
-            if not data['entities'] and not data['predicted_entities']:
-                print(data['text'])
-        else:
-            raise ValueError("Options are 1, 2, 3. Please choose again")
+    if json_file.split('/')[-1].split('.')[0].split('_')[0] == 'intent':
+        for data in datas:
+            print(data['text'] + '\t||\t' + data['intent'] + '\t||\t' + data['intent_prediction']['name'])
+    elif json_file.split('/')[-1].split('.')[0].split('_')[0] == 'DIETClassifier':
+        for data in datas:
+            if options == 1:
+                if not data['predicted_entities']:
+                    print(data['text'])
+            elif options == 2:
+                if not data['entities']:
+                    print(data['text'])
+            elif options == 3:
+                if not data['entities'] and not data['predicted_entities']:
+                    print(data['text'])
+            else:
+                raise ValueError("Options are 1, 2, 3. Please choose again")
+
+def get_data_results():
+    pass
 
 def create_table(json_file, save_options=0):
     with open(json_file, 'r') as f:
         datas = json.load(f)
         
-    dataframe = pd.DataFrame.from_dict(datas, orient='index', columns=['run_1', 'run_2', 'run_3']) #.drop(columns=['support','confused_with'])
+    dataframe = pd.DataFrame.from_dict(datas, orient='index').drop(columns=['confused_with'])
     if save_options == 1:
         dfi.export(dataframe,"mytable.png")
     elif save_options == 0:
@@ -45,6 +51,6 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--save", help="Save figure of results, default", type=int)
     args = parser.parse_args()
 
-    #get_data(args.data, args.options)
+    # get_data_errors(args.data, args.options)
     table = create_table(args.data, args.save)
     print(table)
